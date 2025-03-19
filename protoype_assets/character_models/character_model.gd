f896debe36f94d9dda6948b0912e097a5f6bb0b4
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-03-19 14:41:43 +0100

    added quaternius prototype assets

---
+++ protoype_assets/character_models/character_model.gd
@@ -0,0 +1,8 @@
+class_name CharacterModel
+extends Node3D
+
+@export var animation_player: AnimationPlayer
+@export var animation_tree: AnimationTree
+
+func play_animation(velocity: Vector3, is_on_floor: bool) -> void:
+	pass

ad15469c7b3f7ceb1d1a8f80436a526da236cac9
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-03-21 11:40:23 +0100

    added serialization util

---
+++ utils/serialization.gd
@@ -0,0 +1,7 @@
+class_name Serialization
+
+static func encode_data(value: Variant, full_objects := false) -> String:
+	return JSON.stringify(JSON.from_native(value, full_objects))
+
+static func decode_data(string: String, allow_objects := false) -> Variant:
+	return JSON.to_native(JSON.parse_string(string), allow_objects)

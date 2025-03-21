cfe53d547082591f3490de5b0c77aa86ede4d497
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-03-21 17:50:24 +0100

    added ghost resource

--- characters/character.gd
+++ characters/character.gd
@@ -4,13 +4,25 @@ extends Resource
 @export var name: String
 @export_color_no_alpha var color: Color
 @export var movement_attributes: MovementAttributes
-@export var character_model: PackedScene
 
-var _character_scene: PackedScene = preload("res://characters/character.tscn")
+@export var character_scene: PackedScene = preload("uid://dyhahthciwxpq")
+
+@export_category("World Character")
+@export var _world_character: PackedScene
+@export var _random_world_characters: Array[PackedScene]
+
 
 func create() -> CharacterController:
 	assert(movement_attributes)
-	assert(character_model)
-	var character_controller: CharacterController = _character_scene.instantiate()
+	# XOR operator; either specific character XOR a random character
+	assert(_world_character != null != not _random_world_characters.is_empty())
+	var character_controller: CharacterController = character_scene.instantiate()
 	character_controller.character = self
 	return character_controller
+
+func get_world_character() -> WorldCharacter:
+	# XOR operator; either specific character XOR a random character
+	assert(_world_character != null != not _random_world_characters.is_empty())
+	if _world_character: return _world_character.instantiate()
+	var random_world_character: PackedScene = _random_world_characters.pick_random()
+	return random_world_character.instantiate()

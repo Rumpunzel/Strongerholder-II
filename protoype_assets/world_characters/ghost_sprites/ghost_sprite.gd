e1d40db829d7cad911889f32f22161491aeb46e0
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-03-21 20:09:08 +0100

    synced random ghost sprites

--- protoype_assets/world_characters/ghost_sprites/ghost_sprite.gd
+++ protoype_assets/world_characters/ghost_sprites/ghost_sprite.gd
@@ -1,10 +1,15 @@
 class_name GhostSprite
 extends WorldCharacter
 
-@onready var animated_sprite: AnimatedSprite3D = %AnimatedSprite3D
+@onready var _animated_sprite: AnimatedSprite3D = %AnimatedSprite3D
+@onready var _random_frame := -1:
+	set(new_random_frame):
+		if new_random_frame < 0: return
+		_random_frame = new_random_frame
+		_animated_sprite.frame = _random_frame
 
 func _ready() -> void:
-	animated_sprite.frame = randi() % 20
+	if _random_frame < 0: _random_frame = randi() % 20
 
 func play_animation(_normalized_velocity: Vector3) -> void:
 	pass

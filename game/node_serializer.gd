11c5f54adf151674af3a1ca73a9986838ded9e0a
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-03-22 16:20:04 +0100

    implemented node serialization

---
+++ game/node_serializer.gd
@@ -0,0 +1,67 @@
+@tool
+class_name NodeSerializer
+extends Node
+
+## Optional MultiplayerSpawner; serializes spawned nodes if supplied
+@export var _multiplayer_spawner: MultiplayerSpawner
+
+func _ready() -> void:
+	add_to_group("NodeSerializers")
+	print(serialize())
+
+func collect_nodes() -> Dictionary[String, Array]:
+	assert(_multiplayer_spawner)
+	var spawnable_scene_count := _multiplayer_spawner.get_spawnable_scene_count()
+	var spawnable_scene_paths: Array[String] = [ ]
+	for index: int in range(spawnable_scene_count):
+		var spawnable_scene_path := _multiplayer_spawner.get_spawnable_scene(index)
+		print(spawnable_scene_path)
+		spawnable_scene_paths.append(spawnable_scene_path)
+	
+	var spawn_node := get_node(_multiplayer_spawner.spawn_path)
+	var nodes_to_serialize: Dictionary[String, Array] = { }
+	for node: Node in spawn_node.get_children():
+		var node_scene_path := node.scene_file_path
+		print(node_scene_path)
+		if not spawnable_scene_paths.has(node_scene_path): continue
+		var node_paths: Array[NodePath] = nodes_to_serialize.get(node_scene_path, [ ])
+		node_paths.append(node.get_path())
+		nodes_to_serialize[node_scene_path] = node_paths
+	
+	return nodes_to_serialize
+
+func parse_nodes(collected_nodes: Dictionary[String, Array]) -> void:
+	assert(_multiplayer_spawner)
+	var spawnable_scene_count := _multiplayer_spawner.get_spawnable_scene_count()
+	var spawnable_scene_paths: Array[String] = [ ]
+	for index: int in range(spawnable_scene_count):
+		var spawnable_scene_path := _multiplayer_spawner.get_spawnable_scene(index)
+		print(spawnable_scene_path)
+		spawnable_scene_paths.append(spawnable_scene_path)
+	
+	for node_scene_path: String in collected_nodes.keys():
+		var node_paths: Array[NodePath] = collected_nodes[node_scene_path]
+		var scene_to_spawn: PackedScene = load(node_scene_path)
+		assert(scene_to_spawn is PackedScene)
+		for node_path: NodePath in node_paths:
+			var node_to_spawn := scene_to_spawn.instantiate()
+			print(node_path)
+			var parent_node_path := node_path.slice(0, -1)
+			print(parent_node_path)
+			var parent_node := get_node(parent_node_path)
+			node_to_spawn.name = node_path.get_name(node_path.get_name_count() - 1)
+			parent_node.add_child(node_to_spawn)
+
+func serialize() -> String:
+	return Serialization.encode_data(collect_nodes())
+
+func deserialize(serialized_dict: String) -> void:
+	var collected_nodes: Dictionary[String, Array] = Serialization.decode_data(serialized_dict)
+	assert(collected_nodes is Dictionary[String, Array])
+	parse_nodes(collected_nodes)
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings := [ ]
+	#if not _multiplayer_synchronizer and _properties_to_serialize.is_empty(): warnings.append("Nothing will be serialized!")
+	#if _multiplayer_synchronizer and not _properties_to_serialize.is_empty(): warnings.append("Only MultiplayerSynchronizer OR property list is allowed!")
+	return warnings
--- game/properties_serializer.gd
+++ game/properties_serializer.gd
@@ -10,24 +10,23 @@ extends Node
 func _ready() -> void:
 	add_to_group("PropertiesSerializers")
 
-func collect_properties() -> Dictionary:
+func collect_properties() -> Dictionary[NodePath, Dictionary]:
 	assert(_multiplayer_synchronizer)
-	var _properties_to_serialize := _multiplayer_synchronizer.replication_config.get_properties()
+	var properties_to_serialize := _multiplayer_synchronizer.replication_config.get_properties()
 	var root_node_path := _multiplayer_synchronizer.root_path
 	var root_node := get_node(root_node_path)
 	
-	var properties_dict := { }
-	for property_path: NodePath in _properties_to_serialize:
+	var properties_dict: Dictionary[NodePath, Variant] = { }
+	for property_path: NodePath in properties_to_serialize:
 		var property_node_path := NodePath(property_path.get_concatenated_subnames())
 		var property_value: Variant = root_node.get_indexed(property_node_path)
 		properties_dict[property_node_path] = property_value
 	
 	return { root_node_path: properties_dict }
 
-func parse_properties(collected_properties: Dictionary) -> void:
+func parse_properties(collected_properties: Dictionary[NodePath, Dictionary]) -> void:
 	assert(_multiplayer_synchronizer)
-	var root_node_path := _multiplayer_synchronizer.root_path
-	var root_node := get_node(root_node_path)
+	var root_node := get_node(_multiplayer_synchronizer.root_path)
 	
 	for property_path: NodePath in collected_properties.keys():
 		var property_node_path := NodePath(property_path.get_concatenated_subnames())
@@ -38,8 +37,8 @@ func serialize() -> String:
 	return Serialization.encode_data(collect_properties())
 
 func deserialize(serialized_dict: String) -> void:
-	var collected_properties: Dictionary = Serialization.decode_data(serialized_dict)
-	assert(collected_properties is Dictionary)
+	var collected_properties: Dictionary[NodePath, Dictionary] = Serialization.decode_data(serialized_dict)
+	assert(collected_properties is Dictionary[NodePath, Dictionary])
 	parse_properties(collected_properties)
 
 func _get_configuration_warnings() -> PackedStringArray:

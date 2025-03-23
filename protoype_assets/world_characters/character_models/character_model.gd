c231643af4ddd35cd0a62a010b885ba32d5e7e6d
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-03-23 21:18:28 +0100

    cleanup

--- protoype_assets/world_characters/character_models/character_model.gd
+++ protoype_assets/world_characters/character_models/character_model.gd
@@ -1,7 +1,10 @@
+@tool
 class_name CharacterModel
 extends WorldCharacter
 
-@onready var _animation_tree: AnimationTree = %AnimationTree
+@export_group("Configuration")
+@export var _animation_tree: AnimationTree
+
 @onready var _state_machine: AnimationNodeStateMachinePlayback = _animation_tree["parameters/playback"]
 
 func play_animation(normalized_velocity: Vector3) -> void:
@@ -10,3 +13,8 @@ func play_animation(normalized_velocity: Vector3) -> void:
 		_animation_tree.set("parameters/Walk/blend_position", normalized_velocity.length_squared())
 	else:
 		_state_machine.travel("Idle")
+
+func _get_configuration_warnings() -> PackedStringArray:
+	var warnings: PackedStringArray = [ ]
+	if not _animation_tree: warnings.append("Missing AnimationTree reference.")
+	return warnings

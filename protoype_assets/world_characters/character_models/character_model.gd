17f820d6d916d6e27e2075deb7d53a317716d158
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-03-25 20:18:20 +0100

    cleanups

--- protoype_assets/world_characters/character_models/character_model.gd
+++ protoype_assets/world_characters/character_models/character_model.gd
@@ -2,11 +2,19 @@
 class_name CharacterModel
 extends WorldCharacter
 
+@onready var _meshes: Array[MeshInstance3D] = gather_all_geometry_instances_on(self)
+
 @export_group("Configuration")
-@export var _meshes: Array[MeshInstance3D]
 @export var _animation_tree: AnimationTree
 
 @onready var _state_machine: AnimationNodeStateMachinePlayback = _animation_tree["parameters/playback"]
+
+static func gather_all_geometry_instances_on(node: Node) -> Array[MeshInstance3D]:
+	var geometry_instances: Array[MeshInstance3D] = [ ]
+	for child: Node in node.get_children():
+		if child is MeshInstance3D: geometry_instances.append(child)
+		geometry_instances.append_array(gather_all_geometry_instances_on(child))
+	return geometry_instances
 
 func play_animation(normalized_velocity: Vector3) -> void:
 	if normalized_velocity:
@@ -25,6 +33,5 @@ func apply_material_overlay(material: Material) -> void:
 
 func _get_configuration_warnings() -> PackedStringArray:
 	var warnings: PackedStringArray = [ ]
-	if _meshes.is_empty(): warnings.append("No meshes referenced.")
 	if not _animation_tree: warnings.append("Missing AnimationTree reference.")
 	return warnings + super._get_configuration_warnings()

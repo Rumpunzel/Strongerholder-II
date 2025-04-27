dfd31d93806b5f1a6fab87c0083faded07f2b61d
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-04-27 21:33:09 +0200

    continued menagerie plugin

--- addons/menagerie/menagerie_panel.gd
+++ addons/menagerie/menagerie_panel.gd
@@ -2,3 +2,8 @@
 @icon("uid://b0s3flceyjwl0")
 class_name MenageriePanel
 extends PanelContainer
+
+@export var _preview: Node3D
+
+func _on_visibility_changed() -> void:
+	_preview.visible = visible

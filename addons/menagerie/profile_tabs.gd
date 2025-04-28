7945a0924537d1462f0a5ffec598cf9d8e596472
Jamal Bencharki <bencharki.jamal@gmail.com>
2025-04-28 12:17:04 +0200

    cleanups

---
+++ addons/menagerie/profile_tabs.gd
@@ -0,0 +1,7 @@
+@tool
+extends TabContainer
+
+func _ready() -> void:
+	set_tab_button_icon(0, preload("uid://btd64iwc2p3sh"))
+	set_tab_button_icon(1, preload("uid://bes0anop2dh5u"))
+	set_tab_button_icon(2, preload("uid://c4udocqr7qeyj"))
